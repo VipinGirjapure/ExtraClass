58090e4823aac16447931815622fa22c6e8bf2f7
VIPIN <96302356+VipinGirjapure@users.noreply.github.com>
2022-04-06 13:21:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+// Q 1 : Given array of students marks [23, 45, 67, 78, 91],  add 10 in all the elements of an array by using map and forEach function 
+
+
+// let arr =[23, 45, 67, 78, 91]
+
+// const arr2 = arr.map((a) => {
+//    return a= a+10 
+// })
+// console.log(arr2)
+
+// let arr = [23, 45, 67, 78, 91]
+
+// arr.forEach((a)=>{
+//     arr.map((a) => {
+//         //    return a= a+10 
+//  })
+//  console.log(arr);
+
+
+
+
+
+
+
+// Q 2 : Given array of student marks [23, 45, 67, 78, 91], find out how many students have got marks less than 65 and return the array.
+
+// let marks= [23, 45, 67, 78, 91]
+// let result = marks.filter(checkMarks)
+// function checkMarks(mark) {
+//     return mark < 65; }
+//     console.log(result)
+
+// Q 3 : Return the sum of all the marks of students present in given array
+
+	// var arr =  [23, 45, 67, 78, 91]
+	// var sum = 0;
+	// for (let i = 0; i < arr.length; i++) {
+	// 	sum += arr[i];
+	// }
+	// console.log("Sum is " + sum) 
+
+
+
+
+// Q 4 : Replace ""Peter"" with ""John"" in whole string : Given String : ""Peter is a good friend of Mike. Peter is working in a IT company in Australia"".
+
+// let text = "Peter is a good friend of Mike. Peter is working in a IT company in Australia";
+// let result = text.replaceAll("Peter", "John");
+// console.log(result)
+
+// Q 5 : Reverse a given string ""Hello this is me"" , result should be ""me is this Hello"".
+
+// JavaScript program to reverse a string
+// var s = ["i", "like", "this", 
+//          "program", "very", "much"];
+                                          
+// var ans ="";
+// for (var i = 5; i >= 0; i--)
+// {
+//     ans += s[i] + " ";
+// }
+// document.write(
+// "Reversed String:"+ "<br>");
+// document.write(
+// ans.slice(0,ans.length-1));
+
+// Q 6 : Create object by all 3 possible ways and explain with example
+
+1.
+
+// New Operator
+// Q 7 : Create an object contain name age salary as the properties, retrieve key and values in separate arrays
+// Q 8 : Explain call bind and apply in JS
+// Q 9 : Explain Prototype Chaining "
+
